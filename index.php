621f741650dffb7ef50035e9545b08c6ec28f531
sadhanagsrm14 <57868716+sadhanagsrm14@users.noreply.github.com>
2020-02-27 09:19:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'rkumargupta798@gmail.com';                     // SMTP username
-    $mail->Password   = 'sadhanagupta@123';                               // SMTP password
+    $mail->Password   = '*******';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
